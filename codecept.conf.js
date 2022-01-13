fa591928c2a724de70ad6c19d8ad1dabc9bc2be8
SudhakarKadi <skadi@motoinsight.com>
2022-01-13 18:34:12 +0530

    Just Checking

---
+++ codecept.conf.js
@@ -0,0 +1,37 @@
+require('ts-node/register')
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { bootstrap } = require('./presettings.ts');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './tests/**_test.ts',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://github.com',
+      show: false,
+      windowSize: '1200x900',
+      browser: 'chromium'
+    },
+    CustomHelper: {
+      require: './CustomHelper.ts'
+    }
+  },
+  bootstrap,
+  include: {
+    loginPage: './loginPage.ts',
+    homePage: './homePage.ts'
+  },
+  name: 'typescript-boilerplate',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
